660bdbd0acd8a11322e4fa618482ceee39111313
panupong.ta <panucsvip29@gmail.com>
2025-05-27 16:58:43 +0700

    feat: update model

--- internal/domain/entity/user.go
+++ internal/domain/entity/user.go
@@ -1,9 +1,13 @@
 package entity
 
+import "time"
+
 type User struct {
-	ID       uint
-	Username string
-	Name     string
-	Email    string
-	Password string
+	ID        uint      `gorm:"primaryKey"`
+	Username  string    `gorm:"unique;not null"`
+	Name      string    `gorm:"not null"`
+	Email     string    `gorm:"unique;not null"`
+	Password  string    `gorm:"not null"`
+	CreatedAt time.Time `gorm:"autoCreateTime"`
+	UpdatedAt time.Time `gorm:"autoUpdateTime"`
 }
